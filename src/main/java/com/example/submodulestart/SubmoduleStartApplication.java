66e3ceac5c3738f9a11f57954a8a1790d6174a8e
BEOMJUN KIM <skyblue300a@gmail.com>
2020-07-25 15:43:21 +0900

    initial commit

---
+++ src/main/java/com/example/submodulestart/SubmoduleStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.submodulestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduleStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmoduleStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/submodulestart/SubmoduleStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submodulestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmoduleStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
